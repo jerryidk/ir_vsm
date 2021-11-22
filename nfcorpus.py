3da43dd94432b0b24eaf05f6599a912dd283c886
pjlake98 <24646452+pjlake98@users.noreply.github.com>
2021-11-22 12:48:49 -0700

    Fix writing files for Galago

--- nfcorpus.py
+++ nfcorpus.py
@@ -1,20 +1,22 @@
 import buildindex
 import vsm
+import lsi
 
 pathToCorpus = '../nfcorpus/dev.docs'
 pathToQuery = '../nfcorpus/dev.all.queries'
 pathToResults = './results'
 idx = buildindex.Index(pathToCorpus)
-model = vsm.VSM(idx, 'tflog')
+model = vsm.VSM(idx, 'tflog', './dev_tflog_tfidf.npy')
+model = lsi.LSI(model)
 
 with open(pathToResults, 'w') as results:
     with open(pathToQuery, 'r') as queries:
         for q in queries:
             qid = q.split()[0]
-            res = model.evaluateQuery(q, 3)
+            res = model.evaluateQuery(q, 10)
             rank = 1
             for doc in res:
-                results.write("{}, Q0, {}, {}, galago\n".format(qid, doc, rank))
+                results.write("{} Q0 {} {} -Infinity galago\n".format(qid, doc, rank))
                 rank += 1
 
 
